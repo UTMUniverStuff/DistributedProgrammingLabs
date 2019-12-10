ec6124b280eb589fad213b599a0bcde151441346
Emil Terman <terman.emil@gmail.com>
2019-12-10 10:21:33 +0200

    lab2 - setup

---
+++ lab2/Gateway/Gateway.WebApi/WeatherForecast.fs
@@ -0,0 +1,10 @@
+namespace Gateway.WebApi
+
+open System
+
+type WeatherForecast =
+    { Date : DateTime
+      TemperatureC : int
+      Summary : string }
+
+    member this.TemperatureF = 32 + (int (float this.TemperatureC / 0.5556))
